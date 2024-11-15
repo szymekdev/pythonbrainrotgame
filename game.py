57f08d84ac1713ef6976491a20f75611787b3b1e
szymekdev <sporzukk@gmail.com>
2024-11-15 23:02:48 +0100

    Add files via upload

---
+++ game.py
@@ -0,0 +1,15 @@
+import random 
+import os 
+
+# this is for those who know
+number = random.randint(1,10)
+guess = input(" all the sigmas put number between 1 and 10 ")
+guess = int (guess)
+
+# mewing 
+
+if guess == number: 
+    print("+10000 aura")
+else:
+    print("-500000 aura")
+    print("no more gyats for u")
